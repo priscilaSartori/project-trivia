f639cede23814691d180fc8a8e0e72ffe31aa8b8
Vinicius Herminio Gama da Silva <viniciusherminio1@outlook.com>
2022-11-09 19:08:25 -0300

    css pag login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from '../trivia.png';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class Login extends React.Component {
   state = {
@@ -31,30 +32,48 @@ class Login extends React.Component {
   render() {
     const { disabled, name, email } = this.state;
     return (
-      <div>
+      <div className="position-absolute top-50 start-50 translate-middle">
         <img src={ logo } className="App-logo" alt="logo" />
-        <label htmlFor="inputName">
-          Nome:
-          <input
-            id="inputName"
-            data-testid="input-player-name"
-            name="name"
-            value={ name }
-            onChange={ this.handleChange }
-          />
-        </label>
-        <label htmlFor="inputEmail">
-          Email:
-          <input
-            id="inputEmail"
-            type="email"
-            name="email"
-            data-testid="input-gravatar-email"
-            value={ email }
-            onChange={ this.handleChange }
-          />
-        </label>
-        <button data-testid="btn-play" type="button" disabled={ disabled }>Play</button>
+        <div className="mt-5">
+          <div className="mb-2 d-grid gap-2">
+            <label htmlFor="inputEmail" className="form-label">
+              <input
+                id="inputEmail"
+                type="email"
+                name="email"
+                data-testid="input-gravatar-email"
+                value={ email }
+                onChange={ this.handleChange }
+                className="form-control"
+                placeholder="Qual é o seu e-mail do gravatar?"
+              />
+            </label>
+          </div>
+          <div className="mb-2 d-grid gap-2">
+            <label htmlFor="inputName" className="form-label">
+              <input
+                id="inputName"
+                data-testid="input-player-name"
+                name="name"
+                value={ name }
+                onChange={ this.handleChange }
+                className="form-control"
+                placeholder="Qual é o seu nome?"
+              />
+            </label>
+          </div>
+          <div className="d-grid gap-2">
+            <button
+              data-testid="btn-play"
+              type="button"
+              className="btn btn-success"
+              disabled={ disabled }
+            >
+              Play
+
+            </button>
+          </div>
+        </div>
       </div>
     );
   }
