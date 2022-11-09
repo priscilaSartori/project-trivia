64ea1649874f05a90a97afd1af1ebd9348e07d25
Joseane Oliveira <joseaneoliveira.joli@gmail.com>
2022-11-09 18:54:16 -0300

    Requisito 2

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux';
 import userReducer from './userReducer';
 import playerReducer from './playerReducer';
+import tokenReducer from './tokenReducer';
 
-const rootReducer = combineReducers({ userReducer, playerReducer });
+const rootReducer = combineReducers({ userReducer, playerReducer, tokenReducer });
 
 export default rootReducer;
