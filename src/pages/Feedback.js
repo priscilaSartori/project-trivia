ed988bc46080d5862604ac4cf262b238afe7ddc4
Raoni Andrade <raonio.andrade@gmail.com>
2022-11-10 16:07:31 -0300

    Requisito 12 quase pronto

---
+++ src/pages/Feedback.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from '../components/Header';
+
+class Feedback extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <p>Feedback</p>
+      </div>
+    );
+  }
+}
+
+export default Feedback;
