3dbaddc90b7929be32f8655e10af3731d0d342a1
Joseane Oliveira <joseaneoliveira.joli@gmail.com>
2022-11-11 18:40:20 -0300

    Requisito 15 e 16

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -9,7 +9,7 @@ class Ranking extends React.Component {
   render() {
     return (
       <div>
-
+        <h3 data-testid="ranking-title">Ranking</h3>
         <button
           type="button"
           data-testid="btn-go-home"
