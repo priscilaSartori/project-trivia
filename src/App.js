b4e29ee96e08c0559baa9416d5d9ae19c4c9007c
Joseane Oliveira <joseaneoliveira.joli@gmail.com>
2022-11-11 19:18:35 -0300

    Req 15 e 16 Correções

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Feedback from './pages/Feedback';
 import Game from './pages/Game';
 import Login from './pages/Login';
 import Settings from './pages/Settings';
+import Ranking from './pages/Ranking';
 
 export default function App() {
   return (
@@ -14,6 +15,7 @@ export default function App() {
         <Route path="/game" component={ Game } />
         <Route path="/settings" component={ Settings } />
         <Route path="/feedback" component={ Feedback } />
+        <Route path="/ranking" component={ Ranking } />
       </Switch>
     </div>
   );
